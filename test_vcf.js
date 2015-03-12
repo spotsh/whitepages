a888a1af8abed2cb635feb2135015cec590c341a
Karl Glatz <carlos22b@gmail.com>
2015-03-12 23:12:32 +0100

    quick and dirty addition of single vcf file as data backend; needs still better mapping and stuff

---
+++ test_vcf.js
@@ -0,0 +1,19 @@
+var Gravatar = require('./lib/gravatar').Gravatar,
+    white = require('./lib/whitepages.js'),
+    crypto = require('crypto'),
+    WhitePages = white.WhitePages,
+    VcfBook = white.VcfBook;
+
+
+var wp = new WhitePages('o=whitepages');
+var h = crypto.createHmac('sha256', 'wp');
+h.update('foo');
+wp.hash_key = 'wp';
+wp.password = h.digest('hex');
+
+var book = new VcfBook('test.vcf');
+wp.read(book);
+wp.listen(1389, '0.0.0.0', function(){
+     console.log("White pages at %s", wp.ldap.url);
+});
+
